46514077091a39e57c23104f270d68c47028ca1e
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2024-02-18 15:15:05 +0300

    feat(*): add wrapper for calculateArea function

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const getArea = (func, a, b, intervalStep = 0.001) => {
+// Основной алгоритм Римановского интеграла - дробим ось X на предельно маленькие промежутки, ...
+// ...считаем площади прямоугольников, их сумма = интегральная сумма.
+const calculateArea = (func, a, b, intervalStep = 0.001) => {
   if (
     typeof func !== "function" ||
     typeof a !== "number" ||
     typeof b !== "number"
-  )
+  ) {
     throw new Error(
       "Expected: func - math function, e.g (x: number) => x ** 2; a, b - interval [a: number, b: number]"
     );
+  }
 
   let intervalsOrder = b > a ? 1 : -1;
   let sumSquares = 0;
@@ -22,5 +25,23 @@ const getArea = (func, a, b, intervalStep = 0.001) => {
     sumSquares += intervalStep * ((func(i) + func(i + intervalStep)) / 2);
   }
 
-  return (intervalsOrder * sumSquares).toFixed(3);
+  return (intervalsOrder * sumSquares).toFixed(5);
+};
+
+// Обертка для основной функции, просто есть возможность считать интеграл с разными шагами
+// Чем меньше шаг, тем менее точный результат
+const calculateAreaWithDifferentIntervalSteps = (intervalSteps) => {
+  if (!Array.isArray(intervalSteps)) {
+    throw "Expected array intervalSteps of numbers";
+  }
+
+  return (...args) => {
+    return intervalSteps.reduce(
+      (result, intervalStep) => ({
+        ...result,
+        [`Шаг ${intervalStep}`]: calculateArea(...args, intervalStep),
+      }),
+      {}
+    );
+  };
 };
